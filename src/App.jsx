b3b67679a336d73c0b9c93d38a0d475d2db8a4bc
Chrystalio <chrystalio.k@outlook.com>
2023-07-16 20:45:51 +0700

    Temporary commit, checkbox not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,68 @@
+import { useState } from "react";
 import "./style.css"
 
 
 export default function App(){
+  const [newItem, setNewItem, ] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false }
+      ]
+    })
+
+    setNewItem("");
+  }
+
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+      })
+    })
+  }
+  
   return (
     <>
-      <form action="" className="new-item-form">
+      <h1 className="text-center">Todoify</h1>
+
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" name="item" />
+          <label htmlFor="item">New Item :</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" name="item" />
         </div>
         <button className="btn">
           Add Item
         </button>
       </form>
 
-      <h1 className="header">To Do List</h1>
+      <h1 className="header">To Do List :</h1>
       <ul className="list">
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" name="" id="" /> Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+          {todos.map(todo => {
+            return (
+            <li key={todo.id}>
+              <label htmlFor="">
+                <input
+                  type="checkbox"
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                  checked={todo.completed}
+                />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+          })}
       </ul>
+
+      <p className="footer-text">Made By <a href="https://github.com/chrystalio"><u>Kristoff</u></a></p>
     </>
   );
-};
+}
